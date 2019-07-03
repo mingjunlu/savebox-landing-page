8a736462ce89a7bada596f6fe0e610932086d526
mingjun <40516784+mingjunlu@users.noreply.github.com>
2019-07-03 22:24:54 +0800

    Fix an issue where animations aren't shown in IE 11

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 // Lazy load animations
-const startObserving = () => {
+function startObserving() {
     const elementsToBeRevealed = document.querySelectorAll('[class*="to-be"]')
-    const onObserve = (entries, observer) => {
-        entries.forEach(entry => {
+    const onObserve = function(entries, observer) {
+        entries.forEach(function(entry) {
             if (entry.intersectionRatio > 0) {
                 // 處理 slide-in 效果
-                const classes = entry.target.classList.value.split(' ')
+                const classes = entry.target.classList.toString().split(' ')
                 if (classes.includes('to-be-slided-in')) {
                     const direction = entry.target.dataset.direction
-                    const targetClass = classes.find(cls => cls.startsWith('to-be-slided-in'))
-                    const newClass = targetClass.replace('to-be-slided-in', `slide-in-${direction}`)
-                    entry.target.classList.replace(targetClass, newClass)
+                    const tempArr = classes.concat([])
+                    const classIdx = tempArr.indexOf('to-be-slided-in')
+                    tempArr.splice(classIdx, 1, ('slide-in-' + direction))
+                    entry.target.className = tempArr.join(' ')
                 }
 
                 // 處理 fade-in 效果
@@ -28,13 +29,11 @@ const startObserving = () => {
         threshold: 0.18,
     })
     observer.POLL_INTERVAL = 100
-    elementsToBeRevealed.forEach(element => { observer.observe(element) })
+    elementsToBeRevealed.forEach(function(element) { observer.observe(element) })
 }
 
 // Enable smooth scrolling
 const scroll = new SmoothScroll('a[data-scroll]', { speed: 300 })
 
 // 頁面載入完成後再開始監控有動畫效果的元件，等畫面滑到才開始動畫
-window.addEventListener('load', () => {
-    startObserving()
-})
+window.addEventListener('load', function() { startObserving() }, { once: true })
